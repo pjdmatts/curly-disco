a7b227def63ceb744539e489bb3ffe0aae904a69
Peter Matthews <pjdmatthews7@gmail.com>
2018-02-05 17:33:56 -0500

    Begin CRUD

--- vagrant/catalog/project.py
+++ vagrant/catalog/project.py
@@ -47,22 +47,30 @@ def showItem(category_id, item_id):
     item = session.query(Item).filter_by(id=item_id).one()
     return render_template("publicitem.html", category=category, item=item)
 
-@app.route('/catalog/add', methods=['GET', 'POST'])
-def addItem():
-    # if 'username' not in login_session:
-    #     return redirect('/login')
-    # if request.method == 'POST':
-    #     newItem = Item(name=request.form['name'], user_id=login_session['user_id'])
-    #     session.add(newItem)
-    #     flash('New Items %s Successfully Created' % newItem.name)
-    #     session.commit()
-    #     return redirect(url_for('showCatalog'))
-    # else:
-    return render_template('add.html')
+# @app.route('/catalog/add', methods=['GET', 'POST'])
+# def addItem():
+#     categories = session.query(Category).order_by(asc(Category.name))
+#     if request.method == 'POST':
+#         newItem = Item(name=request.form['name'], description=request.form['description'],
+#                         category_id=request.form['category'], user_id=category.user_id)
+#         session.add(newItem)
+#         session.commit()
+#         return redirect(url_for('showCatalog')
+#     else:
+#         return render_template('add.html')
 
-@app.route('/catalog/<int:category_id>/add')
+
+@app.route('/catalog/<int:category_id>/add', methods=['GET', 'POST'])
 def addCategoryItem(category_id):
-    return render_template("add.html")
+    category = session.query(Category).filter_by(id=category_id).one()
+    if request.method == 'POST':
+        newItem = Item(name=request.form['name'], description=request.form['description'],
+                        category_id=category_id, user_id=category.user_id)
+        session.add(newItem)
+        session.commit()
+        return redirect(url_for('showItems', category_id=category_id))
+    else:
+        return render_template('additem.html', category=category)
 
 @app.route('/catalog/<int:category_id>/<int:item_id>/edit')
 def editItem(category_id, item_id):
